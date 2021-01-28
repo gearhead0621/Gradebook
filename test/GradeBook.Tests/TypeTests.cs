da4e3b03323b12b3b58b4611a16c2407ba5b3665
Nick Szkodyn <nszkodyn@gmail.com>
2021-01-27 21:53:56 -0500

    Working through modules

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,6 +5,85 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
+        
+        [Fact]
+        public void Test1()
+        {
+            var x = GetInt();
+            SetInt(ref x);
+
+            Assert.Equal(42, x);
+        }
+        
+        private void SetInt(ref int x)
+        {
+            x = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetName(ref Book book, string name)
+        {
+            book = new Book(name);
+            book.Name = name;
+        }
+        
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+            book.Name = name;
+        }
+        
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+        
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Nick";
+            var upper = MakeUppercase(name);
+
+            Assert.Equal("Nick", name);
+            Assert.Equal("NICK", name);
+        }
+
+        private string MakeUppercase(string parameter)
+        {
+            return parameter.ToUpper();
+        }
+
         [Fact]
         public void GetBookReturnsDifferentObjects()
         {
